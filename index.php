bc318e8a33e6dde59271ac22a5a3c1b65609efeb
Alexandre Faria <alexandreffaria@me.com>
2018-04-02 12:41:53 -0300

    Mudança de resolução 1920x1080 -> 1366x768

--- index.php
+++ index.php
@@ -6,50 +6,51 @@
     <link rel="stylesheet" href="css/estilo.css">
   </head>
   <body>
-    <div id="Table_01">
-    	<div id="id1920x1080-01">
-    		<img src="images/1920x1080_01.jpg" width="1920" height="42" alt="">
+    <div id="Tabela_01">
+    	<div id="xl-1366x768-01">
+    		<img src="images/xl@1366x768_01.gif" width="1366" height="42" alt="">
     	</div>
-    	<div id="id1920x1080-02">
-    		<img src="images/1920x1080_02.jpg" width="1317" height="356" alt="">
+    	<div id="xl-1366x768-02">
+    		<img src="images/xl@1366x768_02.gif" width="938" height="237" alt="">
     	</div>
     	<div id="instagram">
-    		<a href="https://www.instagram.com/idefestival" target="_blank">
-    			<img src="images/instagram.jpg" width="225" height="92" border="0" alt=""></a>
+    		<a href="https://instagram.com/idefestival" target="_blank">
+    			<img src="images/instagram.gif" width="158" height="63" border="0" alt="Instagram Ide Festival"></a>
     	</div>
     	<div id="facebook">
-    		<a href="https://www.facebook.com/idefestival" target="_blank">
-    			<img src="images/facebook.jpg" width="218" height="92" border="0" alt=""></a>
+    		<a href="https://facebook.com/idefestival" target="_blank">
+    			<img src="images/facebook.gif" width="151" height="63" border="0" alt="Facebook Ide Festival"></a>
     	</div>
-    	<div id="id1920x1080-05">
-    		<img src="images/1920x1080_05.jpg" width="160" height="1038" alt="">
+    	<div id="xl-1366x768-05">
+    		<img src="images/xl@1366x768_05.gif" width="119" height="752" alt="">
     	</div>
-    	<div id="id1920x1080-06">
-    		<img src="images/1920x1080_06.jpg" width="443" height="264" alt="">
+    	<div id="xl-1366x768-06">
+    		<img src="images/xl@1366x768_06.gif" width="309" height="174" alt="">
     	</div>
-    	<div id="id1920x1080-07">
-    		<img src="images/1920x1080_07.jpg" width="909" height="682" alt="">
+    	<div id="xl-1366x768-07">
+    		<img src="images/xl@1366x768_07.gif" width="656" height="515" alt="">
     	</div>
     	<div id="youtube">
-        <iframe width="851" height="487" src="https://www.youtube.com/embed/KJ30sS6crxI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <iframe width="591" height="347" src="https://www.youtube.com/embed/KJ30sS6crxI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     	</div>
-    	<div id="id1920x1080-09">
-    		<img src="images/1920x1080_09.jpg" width="851" height="38" alt="">
+    	<div id="xl-1366x768-09">
+    		<img src="images/xl@1366x768_09.gif" width="591" height="12" alt="">
     	</div>
-    	<div id="id1920x1080-10">
-    		<img src="images/1920x1080_10.jpg" width="303" height="157" alt="">
+    	<div id="xl-1366x768-10">
+    		<img src="images/xl@1366x768_10.gif" width="213" height="156" alt="">
     	</div>
-    	<div id="primeira-sessao">
+    	<div id="sympla-um">
     		<a href="http://bit.ly/idefestivalinfantil" target="_blank">
-    			<img src="images/primeira-sessao.jpg" width="291" height="97" border="0" alt=""></a>
+    			<img src="images/sympla-um.gif" width="193" height="65" border="0" alt="Sympla Ide Festival"></a>
     	</div>
-    	<div id="segunda-sessa">
+    	<div id="sympla-dois">
     		<a href="http://bit.ly/idefestivalconference" target="_blank">
-    			<img src="images/segunda-sessa.jpg" width="257" height="97" border="0" alt=""></a>
+    			<img src="images/sympla-dois.jpg" width="185" height="65" border="0" alt="Sympla Ide Festival"></a>
     	</div>
-    	<div id="id1920x1080-13">
-    		<img src="images/1920x1080_13.jpg" width="548" height="60" alt="">
+    	<div id="xl-1366x768-13">
+    		<img src="images/xl@1366x768_13.gif" width="378" height="91" alt="">
     	</div>
     </div>
+  
   </body>
 </html>
