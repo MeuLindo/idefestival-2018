cc931005076cdef3cd29f00f67ed0be9d64899a7
Alexandre Faria <alexandreffaria@me.com>
2018-04-01 21:14:45 -0300

    Push Inicial

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ide Festival</title>
+
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/flexboxgrid.css" />
+    <link rel="stylesheet" href="css/estilo.css" />
+
+
+  </head>
+  <body>
+    <div id="wrapper">
+    	<div id="lg---1024x768-01">
+    		<img src="images/lg-@-1024x768_01.jpg" width="750" height="194" alt="">
+    	</div>
+    	<div id="instagram">
+    	   <a href="https://instagram.com/idefestival" target="_blank"><img src="images/instagram.jpg" width="124" height="59" alt=""></a>
+    	</div>
+    	<div id="facebook">
+    		<a href="https://facebook.com/idefestival" target="_blank"><img src="images/facebook.jpg" width="123" height="59" alt=""></a>
+    	</div>
+    	<div id="lg---1024x768-04">
+    		<img src="images/lg-@-1024x768_04.jpg" width="27" height="597" alt="">
+    	</div>
+    	<div id="lg---1024x768-05">
+    		<img src="images/lg-@-1024x768_05.jpg" width="247" height="135" alt="">
+    	</div>
+    	<div id="lg---1024x768-06">
+    		<img src="images/lg-@-1024x768_06.jpg" width="465" height="403" alt="">
+    	</div>
+    	<div id="youtube">
+        <iframe width="532" height="316" src="https://www.youtube.com/embed/KJ30sS6crxI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+    	</div>
+    	<div id="lg---1024x768-08">
+    		<img src="images/lg-@-1024x768_08.jpg" width="532" height="17" alt="">
+    	</div>
+    	<div id="lg---1024x768-09">
+    		<img src="images/lg-@-1024x768_09.jpg" width="240" height="70" alt="">
+    	</div>
+    	<div id="primeirasessao">
+    		<a href="http://bit.ly/idefestivalinfantil" target="_blank">
+    			<img src="images/primeirasessao.jpg" width="143" height="55" border="0" alt="Entre no Sympla e compre seu Ingresso!"></a>
+    	</div>
+    	<div id="segunda-sessao">
+    		<a href="http://bit.ly/idefestivalconference" target="_blank">
+    			<img src="images/segunda-sessao.jpg" width="149" height="55" border="0" alt="Compre seu ingresso no Sympla!"></a>
+    	</div>
+    	<div id="lg---1024x768-12">
+    		<img src="images/lg-@-1024x768_12.jpg" width="292" height="15" alt="">
+    	</div>
+    </div>
+
+
+    <!-- <div class="container">
+
+    </div> -->
+
+  </body>
+</html>
